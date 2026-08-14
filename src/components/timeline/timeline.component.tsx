121348d3e595d329aead472c29792909b970ff81
Sai Sreenadh <saisrinadhch@gmail.com>
2026-08-14 21:14:28 +0530

    Removed about page timeline animation

--- src/components/timeline/timeline.component.tsx
+++ src/components/timeline/timeline.component.tsx
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React from "react";
 import styles from "./timeline.module.scss";
-import { gsapContext } from "@/app/context";
 
 interface TimelineItem {
 	duration: string;
@@ -14,45 +13,9 @@ interface TimelineProps {
 }
 
 const Timeline: React.FC<TimelineProps> = ({ items }) => {
-	const timelineRef = useRef<HTMLDivElement>(null);
-	const gsap = useContext(gsapContext).gsap;
-
-	useEffect(() => {
-		if (timelineRef.current) {
-			gsap.to(timelineRef.current, {
-				"--timeline-height": "100%",
-				scrollTrigger: {
-					trigger: timelineRef.current,
-					start: "top 50%",
-					end: "bottom 70%",
-					scrub: true,
-				},
-			});
-
-			const containers = timelineRef.current.querySelectorAll(`.${styles.container}`);
-			containers.forEach((container, index) => {
-				gsap.fromTo(
-					container,
-					{ opacity: 0, y: 100 },
-					{
-						opacity: 1,
-						y: 0,
-						scrollTrigger: {
-							trigger: container,
-							start: "top 80%",
-							end: "top 60%",
-							scrub: true,
-						},
-						duration: 1.5,
-						ease: "power2.out",
-					}
-				);
-			});
-		}
-	}, [gsap]);
 
 	return (
-		<div ref={timelineRef} className={styles.timeline}>
+		<div className={styles.timeline}>
 			{items.map((item, index) => (
 				<div key={index} className={`${styles.container} ${index % 2 === 0 ? styles.left : styles.right}`}>
 					<img src={item.image} alt="Amazon" />
